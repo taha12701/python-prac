9440496c984415e73c06b74d79c619f71610ec2c
taha12701 <108870371+taha12701@users.noreply.github.com>
2024-10-23 23:55:59 +0500

    Add files via upload

--- p2.py
+++ p2.py
@@ -1,10 +1,22 @@
 #function questions
 
 #Question # 1
-def len_list(list1):
-  len_lst=len(list1)
-  return len_lst
+# def len_list(list1):
+#   len_lst=len(list1)
+#   return len_lst
 
-lis=len_list(["Taha","Ahmed","Kohli","Bumrah"])
-print(lis)
+# lis=len_list(["Taha","Ahmed","Kohli","Bumrah"])
+# print(lis)
 
+
+#Question # 2
+
+def cal_fact(a):
+  sum = 1 
+  for i in range(1,a+1,1):
+    sum = sum * i
+
+  return sum
+
+fact=cal_fact(5)
+print("The factorial is : ",fact)
