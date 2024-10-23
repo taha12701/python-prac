15d5390de717acf65606ec1df8f1d93faa9a3ee2
taha12701 <108870371+taha12701@users.noreply.github.com>
2024-10-24 00:55:07 +0500

    Add files via upload

--- p2.py
+++ p2.py
@@ -11,12 +11,31 @@
 
 #Question # 2
 
-def cal_fact(a):
-  sum = 1 
-  for i in range(1,a+1,1):
-    sum = sum * i
+# def cal_fact(a):
+#   sum = 1 
+#   for i in range(1,a+1,1):
+#     sum = sum * i
+#   return sum
+# fact=cal_fact(5)
+# print("The factorial is : ",fact)
 
-  return sum
+#Question # 3
 
-fact=cal_fact(5)
-print("The factorial is : ",fact)
+def currency_cnv(usd):
+  pkr=0.0036
+  amount=(usd/pkr)
+  return amount
+
+output=currency_cnv(278.15)
+print(f"The amount in usd is : {output:.2f}","Rs")
+
+#Question # 4
+
+# def currency_cnv(pkr):
+#   usd=278.15
+#   amount=(pkr/usd)
+#   return amount
+
+# amt=int(input("Enter amount in Pkr : "))
+# output=currency_cnv(amt)
+# print(f"The amount in usd is :  {output:.2f}$")
