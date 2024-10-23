895bb61153e2f0d17c31498af92bc90aa4b7d7e4
taha12701 <108870371+taha12701@users.noreply.github.com>
2024-10-23 23:52:08 +0500

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,44 @@
+#functions in python
+
+# def sum(a,b):
+#   sum=a+b
+#   return sum
+
+
+# a=int(input("Enter a number : "))
+# b=int(input("Enter a number : "))
+
+# print("The sum is equal to :", sum(a,b))
+
+
+# def print_hello():
+#   print("Hello World")
+
+
+# for i in range(0,4,1):
+#   print_hello()
+
+
+# def cal_avg(a,b):
+#   sum = a + b
+#   avg = sum/2
+#   print("Average is equal to : ",avg)
+
+# cal_avg(5,8)
+
+
+# print("Syed Taha", end=" ")
+# print("Ahmed")
+
+# print(len("aha"))
+
+
+
+# def cal_sum(a,b=-4):
+#   sum = a+b
+#   print("Sum is equal to :", sum)
+#   return sum
+
+# cal_sum(1)
+
+
---
+++ p2.py
@@ -0,0 +1,10 @@
+#function questions
+
+#Question # 1
+def len_list(list1):
+  len_lst=len(list1)
+  return len_lst
+
+lis=len_list(["Taha","Ahmed","Kohli","Bumrah"])
+print(lis)
+
