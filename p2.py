2bdd650fb160a71232564509c96773a7a1e6dab7
taha12701 <108870371+taha12701@users.noreply.github.com>
2024-10-24 21:14:43 +0500

    Add files via upload

--- p2.py
+++ p2.py
@@ -79,14 +79,67 @@
 
 #Question # 8
 
-def cal_avg(n):
-  n=int(input("Enter no of items : "))
-  sum=0
-  for i in range(0,n,1):
-    k=int(input("Enter a number : "))
-    sum=sum+k
-  print("The sum is equal to : ", sum)
-  avg=(sum)/n
-  print(f"The average is equal to {avg:.2f} ")  
-cal_avg(4)
+# def cal_avg(n):
+#   n=int(input("Enter no of items : "))
+#   sum=0
+#   for i in range(0,n,1):
+#     k=int(input("Enter a number : "))
+#     sum=sum+k
+#   print("The sum is equal to : ", sum)
+#   avg=(sum)/n
+#   print(f"The average is equal to {avg:.2f} ")  
+# cal_avg(4)
+
+#Question # 9
+
+# def str_rev(str1):
+
+# str1="ViratKohli"
+# len_str=len(str1)
+# rev_str=""
+  
+# while len_str>0:
+#   rev_str +=str1[len_str-1]
+#   len_str = len_str -1
+
+# print(rev_str)
+
+# n = int(input("Enter a number: "))
+
+# # Check for non-prime numbers 0 and 1
+# if n == 0 or n == 1:
+#     print(f"{n} is not a prime number")
+
+# else:
+#     # Loop to check divisibility
+#     for i in range(2, n):
+#         if n % i == 0:
+#             print(f"{n} is not a prime number")
+#             break
+#     else:
+#         # If no divisors were found, it's a prime number
+#         print(f"{n} is a prime number")
+
+
+#prime number function
+
+def prime_num():
+  num=int(input("Enter a number : "))
+  if(num==0 or num==1 or num<0):
+    print(f"{num} is not a prime number")
+  else:
+    for i in range(2,num,1):
+      if(num%i==0):
+        print(f"{num} is not a prime number")
+        break
+    else:
+      print(f"{num} is  a prime number")
+
+prime_num()
+
+
+
+        
+  
+
 
